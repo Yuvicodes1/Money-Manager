4d6cca97412f45a357da97d7d65ccae52277c8f5
Yathaartha Srivastava <yuvicodes@gmail.com>
2026-02-14 11:30:12 +0530

    Backend Setup Complete with Connection to MongoBD Atlas

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Stock Monitor API is running...");
+});
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
