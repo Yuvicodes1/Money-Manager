9271dd559bb485035f9cfa6a21b6098f9d94acb4
Yathaartha Srivastava <yuvicodes@gmail.com>
2026-03-07 22:57:10 +0530

    Adding automatic render wake up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,9 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
 import { CurrencyProvider } from "./context/CurrencyContext";
 
+// Wake up Render backend on app load
+fetch("https://fintrack-bld5.onrender.com/").catch(() => {});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
